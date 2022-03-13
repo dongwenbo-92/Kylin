14eebb7cc56226145e33d456b4c40aeb0811c54c
董文博 <dongwenbo1992@163.com>
2022-03-13 13:17:19 +0800

    实现底部标签导航

--- app/src/main/java/com/dongwenbo/kylin/MainActivity.kt
+++ app/src/main/java/com/dongwenbo/kylin/MainActivity.kt
@@ -2,10 +2,24 @@ package com.dongwenbo.kylin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.dongwenbo.kylin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        this.setUpTabNavigation()
+    }
+    private fun setUpTabNavigation() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(binding.bottomNavigationView.menu)
+        NavigationUI.setupActionBarWithNavController(this,navController,appBarConfiguration)
+        NavigationUI.setupWithNavController(binding.bottomNavigationView,navController)
     }
 }
