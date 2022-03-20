3721c5f2a79d419593136476b6b7139167e55cf1
董文博 <dongwenbo1992@163.com>
2022-03-20 17:47:47 +0800

    增加从浏览器回调获取授权码

--- app/src/main/java/com/dongwenbo/kylin/login/LoginActivity.kt
+++ app/src/main/java/com/dongwenbo/kylin/login/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.dongwenbo.kylin.login
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.dongwenbo.kylin.databinding.ActivityLoginBinding
 import com.dongwenbo.kylin.login.api.AuthService
@@ -10,22 +11,35 @@ import com.dongwenbo.kylin.login.utilities.CLIENT_ID
 import com.dongwenbo.kylin.login.utilities.REDIRECT_URI
 import com.dongwenbo.kylin.login.utilities.SCOPE
 
+const val TAG = "LoginActivity"
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var timeStamp: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
         binding.loginBtn.setOnClickListener {
-            this.authorizeFromBrowser(CLIENT_ID, REDIRECT_URI, SCOPE)
+            this.timeStamp = System.currentTimeMillis().toString()
+            this.authorizeFromBrowser(CLIENT_ID, REDIRECT_URI, SCOPE, this.timeStamp)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        //校验随机码
+        val state = intent?.data?.getQueryParameter("state")
+        if (state != null && state == this.timeStamp){
+            val code = intent.data?.getQueryParameter("code")
+            Log.d(TAG, "onResume: $code")
         }
     }
 
     /**
      * 从浏览器授权
-     *
+
      * @param clientId 应用ID
      * @param redirectUri 重定向URI
      * @param scope 授权范围
@@ -40,10 +54,12 @@ class LoginActivity : AppCompatActivity() {
         allowSignup: String = "true"
     ) {
         val link = AuthService.BASE_URL + AuthService.AUTH_PATH
-        val paras = "client_id=$clientId&redirect_uri=$redirectUri&scope=$scope&state=$state&allow_signup=$allowSignup"
+        val paras =
+            "client_id=$clientId&redirect_uri=$redirectUri&scope=$scope&state=$state&allow_signup=$allowSignup"
         val uri: Uri =
             Uri.parse("$link?$paras")
         val intent = Intent(Intent.ACTION_VIEW, uri)
         startActivity(intent)
     }
+
 }
--- app/src/main/java/com/dongwenbo/kylin/login/utilities/AuthConstants.kt
+++ app/src/main/java/com/dongwenbo/kylin/login/utilities/AuthConstants.kt
@@ -1,7 +1,9 @@
 package com.dongwenbo.kylin.login.utilities
 
+import com.dongwenbo.kylin.BuildConfig
+
 const val CLIENT_ID = "cfdb2028ae83fb0160a1"
 
 const val SCOPE = "repo,gist,user,admin"
 
-const val REDIRECT_URI = "kylin://kylinapp/login"
+const val REDIRECT_URI = "kylin://${BuildConfig.APPLICATION_ID}/login"
